8a3421be80ba4a611631432b6b3df268bdbf86e5
Georgiy Bakiev <bakiyev3005@gmail.com>
2025-05-05 21:23:41 +0300

    HomeWork 11

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,53 @@
 public class Main {
+
+    public static String isLeapYear(int year) {
+        String result;
+        if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) {
+            result = year + " год, является високосным";
+        }
+        else {
+            result = year + " год, не является високосным";;
+        }
+        return result;
+    }
+
+    public static String appVersionControl(int typeOs, int clientDeviceYear) {
+        int currentYear = 2025;
+        String result;
+
+        if (typeOs == 0 && clientDeviceYear == currentYear) {
+            result = "Установите версию приложения для iOS по ссылке";
+        } else if (typeOs == 0 && clientDeviceYear < currentYear) {
+            result = "Установите облегченную версию приложения для iOS по ссылке";
+        } else if (typeOs == 1 && clientDeviceYear == currentYear) {
+            result = "Установите версию приложения для Android по ссылке";
+        } else if (typeOs == 1 && clientDeviceYear < currentYear) {
+            result = "Установите облегченную версию приложения для Android по ссылке";
+        } else {
+            result = "Не нашли указаный год, или вид системы";
+        }
+
+        return result;
+    }
+
+    public static String calculateDeliveryDays(int distance) {
+        if (distance <= 0) {
+            return "Некорректное расстояние";
+        } else if (distance > 100) {
+            return "Доставка невозможна";
+        } else if (distance <= 20) {
+            return "Потребуется дней: 1";
+        } else if (distance <= 60) {
+            return "Потребуется дней: 2";
+        } else if (distance <= 100) {
+            return "Потребуется дней: 3";
+        }
+        return "Некорректное расстояние";
+    }
+
     public static void main(String[] args) {
-        // Задача 1
-        int[] salaryPerMonth = new int[] {12_000, 15_000, 17_000, 10_000, 13_000};
-        int money = 0;
-        for (int i = 0; i < salaryPerMonth.length; i++) {
-            money += salaryPerMonth[i];
-        }
-        System.out.println("Сумма трат за месяц составила " + money + " рублей\n");
-
-        // Задача 2
-        int[] spending = new int[] {11_000, 13_000, 15_000, 17_000, 19_000};
-        int maxSpend = -1;
-        int minSpend = spending[0];
-        for (int i = 0; i < spending.length;i++) {
-            if (spending[i] > maxSpend) {
-                maxSpend = spending[i];
-            }
-            if (spending[i] < minSpend) {
-                minSpend = spending[i];
-            }
-        }
-        System.out.println("Минимальная сумма трат за неделю составила " + minSpend + " рублей. Максимальная сумма трат за неделю составила " + maxSpend +" рублей\n");
-
-        // Задача 3
-        int[] average = new int[] {22_000, 32_000, 45_000, 32_213, 56_000};
-        double summa = 0;
-        for (int i = 0; i < average.length; i++) {
-            summa += average[i];
-        }
-        System.out.println("Средняя сумма трат за месяц составила " + (summa / average.length) + " рублей");
-
-        // Задача 4
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i != -1; i--) {
-            System.out.print(reverseFullName[i]);
-        }
+        System.out.println(isLeapYear(2000));
+        System.out.println(appVersionControl(0, 2023));
+        System.out.println(calculateDeliveryDays(20));
     }
 }
