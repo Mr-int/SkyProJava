be6fe1648fa8fa21d685ed71717fdfbe39d0af8a
Gosha <bakiyev3005@gmail.com>
2025-05-12 16:25:09 +0300

    hw-12

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,16 @@
 public class Main {
-  
-    public static String isLeapYear(int year) {
-        String result;
-        if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) {
-            result = year + " год, является високосным";
-        }
-        else {
-            result = year + " год, не является високосным";;
-        }
-        return result;
-    }
-
-    public static String appVersionControl(int typeOs, int clientDeviceYear) {
-        int currentYear = 2025;
-        String result;
-
-        if (typeOs == 0 && clientDeviceYear == currentYear) {
-            result = "Установите версию приложения для iOS по ссылке";
-        } else if (typeOs == 0 && clientDeviceYear < currentYear) {
-            result = "Установите облегченную версию приложения для iOS по ссылке";
-        } else if (typeOs == 1 && clientDeviceYear == currentYear) {
-            result = "Установите версию приложения для Android по ссылке";
-        } else if (typeOs == 1 && clientDeviceYear < currentYear) {
-            result = "Установите облегченную версию приложения для Android по ссылке";
-        } else {
-            result = "Не нашли указаный год, или вид системы";
-        }
-
-        return result;
-    }
-
-    public static String calculateDeliveryDays(int distance) {
-        if (distance <= 0) {
-            return "Некорректное расстояние";
-        } else if (distance > 100) {
-            return "Доставка невозможна";
-        } else if (distance <= 20) {
-            return "Потребуется дней: 1";
-        } else if (distance <= 60) {
-            return "Потребуется дней: 2";
-        } else if (distance <= 100) {
-            return "Потребуется дней: 3";
-        }
-        return "Некорректное расстояние";
-    }
-
     public static void main(String[] args) {
-        System.out.println(isLeapYear(2000));
-        System.out.println(appVersionControl(0, 2023));
-        System.out.println(calculateDeliveryDays(20));
+        Author Author1 = new Author("Aleksandr", "Pushkin");
+        Author Author2 = new Author("Lev", "Tolstoy");
 
-    public static void main(String[] args) {
-        System.out.println("Task 1");
-        String firstName = "Ivan ";
-        String midleName = "Ivanovich ";
-        String lastName = "Ivanov ";
-        String fullName;
-        fullName = lastName + firstName + middleName;
-        System.out.println("Ф.И.О сотрудника - " + fullName + "\n");
+        Book Book1 = new Book("Prorok", Author1, 1828);
+        Book Book2 = new Book("War and Peace", Author2, 1867);
 
-        System.out.println("Task 2");
-        String upperFullName = fullName.toUpperCase();
-        System.out.println("Данные Ф.И.О сотрудника для заполнения отчета " + upperFullName + "\n");
+        System.out.println("Книга " + Book1.getBookName() + " написанная " + Book1.getAuthor() + " в " + Book1.getYearOfMade() + " году");
+        System.out.println("Книга " + Book2.getBookName() + " написанная " + Book2.getAuthor() + " в " + Book2.getYearOfMade() + " году");
 
-        System.out.println("Task 3");
-        fullName = "Иванов Семён Семёнович";
-        fullName = fullName.replace('ё', 'е');
-        System.out.println("Данные Ф.И.О сотрудника " + fullName);
+        System.out.println("Используем setter");
+        Book1.setYearOfMade(2000);
+        System.out.println("Книга " + Book1.getBookName() + " написанная " + Book1.getAuthor() + " в " + Book1.getYearOfMade() + " году");
     }
 }
